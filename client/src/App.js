c2ef88b3c3c739024da677769c888a8449003bd4
Hernan <hernanlogran@hotmail.com>
2020-02-17 21:17:56 -0300

    Implement client's grid layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./App.css";
 
 function App() {
-  return <div className="App">Issue tracker</div>;
+  return (
+    <Container className="mt-5">
+      <Row>
+        <Col>Users ddl will go here</Col>
+      </Row>
+      <Row>
+        <Col>Issues will be displayed here</Col>
+      </Row>
+    </Container>
+  );
 }
-
 export default App;
