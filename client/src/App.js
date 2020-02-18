e4cdc9f9f921569aef368e4a6f1555815e154209
Hernan <hernanlogran@hotmail.com>
2020-02-18 13:39:10 -0300

    Add filtering logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import queryString from "query-string";
 import UsersDropdownList from "./components/UsersDropdownList";
 import Issue from "./components/Issue";
 import "./App.css";
@@ -7,26 +8,42 @@ import "./App.css";
 function App() {
   const [users, setUsers] = useState([]);
   const [issues, setIssues] = useState([]);
+  const [who, setWho] = useState(null);
 
   useEffect(() => {
     (async () => {
+      //get query parameter
+      const { who } = queryString.parseUrl(window.location.href).query;
+      setWho(who);
+
       //fetch users
       let response = await fetch("/users");
       const users = await response.json();
       setUsers(users);
 
       //fetch issues
-      response = await fetch("/issues");
+      const url = who ? `/users/${who}/issues` : "/issues";
+      response = await fetch(url);
       const issues = await response.json();
       setIssues(issues);
     })();
   }, []);
 
+  const onChangeUser = event => {
+    const who = event.target.value;
+    setWho(who);
+    window.location.search = who ? `who=${who}` : "";
+  };
+
   return (
     <Container className="mt-5">
       <Row>
         <Col>
-          <UsersDropdownList users={users} />
+          <UsersDropdownList
+            users={users}
+            selected={who}
+            onChange={onChangeUser}
+          />
         </Col>
       </Row>
       <Row>
--- client/src/components/UsersDropDownList/index.js
+++ client/src/components/UsersDropDownList/index.js
@@ -5,12 +5,18 @@ import "./styles.scss";
 const ALL_USERS_TEXT = "All users";
 
 export default function UsersDropdownList(props) {
-  const { users } = props;
+  const { users, selected, onChange } = props;
 
   return (
     <Form.Group as={Row} className="ml-0 mr-0 align-items-center">
       <Form.Label className="mr-2">Show issues assigned to:</Form.Label>
-      <Form.Control as="select" id="ddlUsers" className="users-drop-down-list">
+      <Form.Control
+        as="select"
+        id="ddlUsers"
+        className="users-drop-down-list"
+        value={selected || ALL_USERS_TEXT}
+        onChange={onChange}
+      >
         <option value={""}>{ALL_USERS_TEXT}</option>
         {users.map(({ login: user }) => (
           <option key={user} value={user}>
